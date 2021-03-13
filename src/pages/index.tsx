a198b887f7dbd4a7220cac3ebad250fd0e816201
Guilherme Baron <floriani.guilherme@gmail.com>
2021-03-13 19:25:04 -0300

    setup rtl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-type Props = {
-  title: string
-}
-
-export default function Home({ title = 'Hello Next.js' }: Props) {
-  return <h1>{title}</h1>
+export default function Hello({ title = 'world' }) {
+  return <h1>hello {title}</h1>
 }
